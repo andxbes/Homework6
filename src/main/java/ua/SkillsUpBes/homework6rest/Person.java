4be1e88e97d69a1741a634454064bb3dd0d5cb0a
Andr <you@example.com>
2014-07-29 03:10:21 +0300

    start of arhitype

---
+++ src/main/java/ua/SkillsUpBes/homework6rest/Person.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.SkillsUpBes.homework6rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author Andr
+ */
+@Path("/persone")
+public class Person {
+
+    private String name ,
+	           address ,
+	           phone ;
+
+    /**
+     * @return the name
+     */
+    public Person() {
+	name = "Empty";
+	address = "Empty";
+        phone = "Empry";
+    }
+    public Person(String name ,String address,String phone) {
+	this.name = name;
+	this.address = address;
+        this.phone = phone;
+    }
+
+    /**
+     * @return the name
+     */
+    
+    
+    public String getName() {
+	return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    /**
+     * @return the address
+     */
+    
+    public String getAdress() {
+	return address;
+    }
+
+    /**
+     * @param adress the address to set
+     */
+    public void setAdress(String adress) {
+	this.address = adress;
+    }
+
+    /**
+     * @return the phone
+     */
+    
+    public String getPhone() {
+	return phone;
+    }
+
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+	this.phone = phone;
+    }
+    
+    // call the path "/persone/show" 
+    @GET
+    @Path("/show")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String show(){
+         return (getAdress()+"\n"+ getName()+"\n"+ getPhone() + "\n");
+    }
+    
+    //call the default path "/persone"
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String show2(){
+         return show();
+    }
+    
+    //return json type 
+    @GET
+    @Path("/getP")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Person getPerson(){
+	Person p;
+	p = this;
+	//p = new Person("Вася Пупкин", "New-York", "102");
+        return p;  
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,8 +1,41 @@
 <html>
+    <script src="http://code.jquery.com/jquery-2.1.1.js"></script>
+
+
+<!--    <script> 
+    function getJson(){
+        $.ajax({
+            type: 'GET',
+            url: "webresources/persone/getP",
+            dataType: 'json',
+            error: function (jqXHR, textStatus, errorThrown) {
+                            $("#outGetJson").html("error: " + textStatus);
+                        },
+            success: function (data,textStatus,jqXHR){
+                KEY  = "phone";
+                var outData = "GET" + KEY + ": " + data[KEY];
+                $("#outGetJson").html(outData);
+            }
+        });
+    }
+    
+    </script>-->
+    
 <body>
 <h2>Jersey RESTful Web Application!</h2>
-<p><a href="webresources/myresource">Jersey resource</a>
-<p>Visit the <a href="http://jersey.java.net">Project Jersey website</a>
-for more information on Jersey!
+
+
+<p><a href="webresources/myresource">Jersey resource</a></p>
+<p><a href="webresources/persone/show">Persone resource "persone/show"</a></p>
+<p><a href="webresources/persone">Persone resource (to default)</a></p>
+<p><a href="webresources/persone/getP">getP (Person Object)</a></p>
+
+<div>
+<input type="submit" value="GetJson" name="GetJson" onclick="getJson()" />
+<a id="outGetJson"> </a>
+</div>
+
+    
+
 </body>
 </html>
